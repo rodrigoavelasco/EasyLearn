d09e570d674ae92378ab282fca6b196413fcc8fb
Rodrigo Velasco <rodrialejandro@Rodrigos-MBP.localhost>
2019-07-13 01:17:41 -0500

    start of project

---
+++ EasyLearn/src/game.java
@@ -0,0 +1,10 @@
+
+public class game {
+
+    private String userInput;
+    private int highScore;
+
+    public static void main(String args){
+        System.out.println("Hello Welcome");
+    }
+}
