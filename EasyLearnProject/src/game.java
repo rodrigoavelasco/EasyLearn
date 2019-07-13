fc8a3ef8d5a4908b743813719ca0aff69467d914
rodrigoavelasco <rodrigovelasco9@gmail.com>
2019-07-13 02:11:35 -0500

    fixed to be able to run

--- EasyLearnProject/src/game.java
+++ EasyLearnProject/src/game.java
@@ -4,7 +4,7 @@ public class game {
     private String userInput;
     private int highScore;
 
-    public static void main(String args){
+    public static void main(String args[]){
         System.out.println("Hello Welcome");
     }
 }
